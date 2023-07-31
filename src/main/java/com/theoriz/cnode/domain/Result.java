9119aa86b1cc67145c023c9529cc90b318945775
theoriz0 <zhang_00710@qq.com>
2023-07-31 09:42:57 +0800

    fix @NonNull on primitive warning

--- src/main/java/com/theoriz/cnode/domain/Result.java
+++ src/main/java/com/theoriz/cnode/domain/Result.java
@@ -2,14 +2,14 @@ package com.theoriz.cnode.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @AllArgsConstructor
 @Data
 public class Result<T> {
-    @NonNull
     private boolean success;
     private T data;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
 }
